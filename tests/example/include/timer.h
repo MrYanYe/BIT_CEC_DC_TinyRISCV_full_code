c95236364c8eccc576d43b712a1f0b91f8928158
MrYanye <18201538120m@sina.cn>
2026-01-02 18:52:11 +0800

    Add some Macros in timer.h

--- tests/example/include/timer.h
+++ tests/example/include/timer.h
@@ -6,6 +6,12 @@
 #define TIMER0_COUNT  (TIMER0_BASE + (0x04))
 #define TIMER0_VALUE  (TIMER0_BASE + (0x08))
 
+// Add by YanZY, 202501021845. Reference from TASK1
+#define TIMER0_I  (TIMER0_BASE + (12))
+#define TIMER0_SUM_RESULT  (TIMER0_BASE + (16))
+#define TIMER0_SUM_CTRL  (TIMER0_BASE + (20))
+#define TIMER0_I_VALUE  (TIMER0_BASE + (24))
+
 #define TIMER0_REG(addr) (*((volatile uint32_t *)addr))
 
 #endif
